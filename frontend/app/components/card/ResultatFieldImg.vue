d5aa092328522ad31dd27de7610be0f297b37596
CApiTaiNn <morgantsloan@gmail.com>
2026-03-24 16:24:38 +0100

    feat: add components for motorcycles comparo

---
+++ frontend/app/components/card/ResultatFieldImg.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const props = defineProps<{
+  fieldName: string
+  firstValue: string
+  secondValue: string
+}>()
+</script>
+
+<template>
+  <div class="resultat">
+    <div class="container-row">
+      <div class="left">
+        <img :src="`${props.firstValue}`" alt="Pas d'image" />
+      </div>
+      <div class="right">
+        <img :src="`${props.secondValue}`" alt="Pas d'image" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h3 {
+  text-align: center;
+}
+
+.resultat {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
+
+.container-row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 80%;
+  gap: 20px;
+}
+
+.left,
+.right {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+</style>
