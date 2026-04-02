e70e33f6c04c69cde2422bb6f58bb066cc347150
nomi <nomibarbotaud26@gmail.com>
2026-04-02 09:42:38 +0200

    adding comment motorcycle detail page

--- frontend/app/pages/motorcycle/[id].vue
+++ frontend/app/pages/motorcycle/[id].vue
@@ -3,12 +3,30 @@ import { onMounted, ref, computed, watch, nextTick } from 'vue'
 import type { IMotorcycle } from '~/types/motorcycles'
 import CountUp from 'vue-countup-v3'
 import type { IMessage } from '~/types/messages'
+import Comment from '~/components/forum/Comment.vue'
+
+interface ICommentInput {
+  motorcycleId: string
+  motorcycleName: string
+  brand: string
+  content: string
+  user: string // TODO: update quand l'auth sera en place
+}
 
 const route = useRoute()
 const id = route.params.id as string
 const apiBase = useRuntimeConfig().public.apiBase
 const m = ref<IMotorcycle | null>(null)
 const commentsMotorcycle = ref<IMessage[]>([])
+const comment = ref<ICommentInput>({
+  motorcycleId: '',
+  motorcycleName: '',
+  brand: '',
+  content: '',
+  user: '69cbe6342e0cabab3167824a' // TODO: update quand l'auth sera en place
+})
+const messagePosted = ref<boolean>(false)
+const isConnected = ref<boolean>(true) // Simule l'état de connexion de l'utilisateur
 
 const statsRef = ref<HTMLElement | null>(null)
 const countStarted = ref(false)
@@ -62,11 +80,11 @@ async function fetchData() {
     }
   )
   m.value = data.motorcycles?.[0] ?? null
+  console.log('Motorcycle data:', m.value)
 }
 
 async function fetchMessages() {
   const post1 = m.value?.post
-
   if (post1) {
     const data = await $fetch<{ messages: IMessage[] }>(
       `${apiBase}posts/${post1}/responses`,
@@ -82,9 +100,58 @@ async function fetchMessages() {
   }
 }
 
-onMounted(() => {
-  fetchData()
-  fetchMessages()
+async function postComment() {
+  if (!comment.value.content || !comment.value.motorcycleId) return
+
+  let postId = m.value?.post
+  if (!postId) {
+    try {
+      const newPost = await $fetch<{ _id: string }>(`${apiBase}posts`, {
+        method: 'POST',
+        body: {
+          title: m.value?.name,
+          brand: m.value?.brand._id,
+          category: 'Modèle',
+          user: comment.value.user,
+          content: `Discussion autour de la ${m.value?.brand.name} ${m.value?.name}`
+        }
+      })
+
+      postId = newPost._id
+      await $fetch(`${apiBase}motorcycles/${m.value?._id}`, {
+        method: 'PUT',
+        body: {
+          post: postId
+        }
+      })
+
+      m.value.post = postId
+    } catch (error) {
+      console.error('Error creating post:', error)
+      return
+    }
+  }
+
+  try {
+    await $fetch(`${apiBase}messages`, {
+      method: 'POST',
+      body: {
+        content: comment.value.content,
+        user: comment.value.user,
+        reference: postId,
+        referenceModel: 'Post'
+      }
+    })
+    messagePosted.value = true
+  } catch (error) {
+    console.error('Error posting comment:', error)
+  }
+  await fetchMessages()
+}
+
+onMounted(async () => {
+  await fetchData()
+  await fetchMessages()
 })
 
 watch(
@@ -145,13 +212,31 @@ watch(
       <p v-else>Pas d'audio</p>
     </div>
 
-    <div>
-      <h4>Commentaires sur la {{ m.name }}</h4>
-      <Comment
-        v-if="commentsMotorcycle.length > 0"
-        :responses="commentsMotorcycle"
-      />
-      <p v-else>Postez le premier commentaire !</p>
+    <div
+      v-if="!messagePosted"
+      class="input-comment-container"
+      :class="{ blurred: !isConnected }"
+    >
+      <h4>
+        Déjà roulé sur cette moto ?<br />
+        Faite le savoir à la communauté !
+      </h4>
+      <div class="comment-input">
+        <UTextarea
+          v-model="comment.content"
+          size="xl"
+          placeholder="Un retour d'expérience, un conseil d'entretient ou encore une question"
+        />
+      </div>
+      <UButton
+        class="rounded-4xl self-end text-xs m-1"
+        size="xl"
+        @click="postComment"
+        >Poster</UButton
+      >
+    </div>
+    <div v-if="commentsMotorcycle.length > 0" class="display-comment">
+      <Comment :responses="commentsMotorcycle" />
     </div>
   </div>
 </template>
@@ -224,5 +309,14 @@ span {
   max-width: 100%;
   height: auto;
   border-radius: 8px;
+}
+
+.display-comment {
+  flex: 1;
+  border: 1px solid #c0c0c0;
+  border-radius: 1.25rem;
+  padding: 2rem;
+  max-width: 50%;
+  height: fit-content;
 }
 </style>
