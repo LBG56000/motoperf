3de5fcb12212f1e0beea9d9730b10debc9b1edb1
CApiTaiNn <morgantsloan@gmail.com>
2026-04-08 10:58:40 +0200

    feat: responsive on caroussel

--- frontend/app/components/CarrouselMotorcycles.vue
+++ frontend/app/components/CarrouselMotorcycles.vue
@@ -19,21 +19,20 @@ const emit = defineEmits<{
     :items="items"
     :ui="{
       container: 'flex items-stretch h-full',
-      item: 'basis-1/5 ps-0 self-stretch',
-      prev: 'sm:start-0',
-      next: 'sm:end-0'
+      item: 'basis-1/2 sm:basis-1/3 lg:basis-1/5 ps-0 self-stretch',
+      prev: 'start-0',
+      next: 'end-0'
     }"
   >
     <article
       class="cursor-pointer"
       @click="navigateTo(`/motorcycle/${item._id}`)"
     >
-      <h5 class="no-select">{{ item.name }}</h5>
+      <h5 class="no-select h5-mobile">{{ item.name }}</h5>
       <img
         src="/images/motorcycles/YZF-R1.png"
         width="100"
         height="100"
-        c
         class="rounded-lg"
         loading="lazy"
       />
@@ -49,6 +48,7 @@ const emit = defineEmits<{
           size="sm"
           color="primary"
           class="rounded-full"
+          cursor-pointer
           style="color: white"
           icon="i-lucide-arrow-left-right"
           @click.stop="
@@ -63,6 +63,7 @@ const emit = defineEmits<{
 
 <style scoped>
 article {
+  cursor: pointer;
   display: flex;
   flex-direction: column;
   text-align: center;
@@ -100,10 +101,20 @@ hr {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: -5px;
+  gap: -5px
 }
 
 .no-select {
   user-select: none;
+}
+
+
+@media (max-width: 1024px) {
+  article {
+    width: 180px;
+  }
+  img {
+    width: 70px;
+  }
 }
 </style>
