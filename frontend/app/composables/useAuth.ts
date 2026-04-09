4b8dbe35b02846594aa2312686c5b45080e44577
mini_bzh <mini.h.13.9.14.9@gmail.com>
2026-04-09 15:34:57 +0300

    fix: register&updateProfile in useAuth

--- frontend/app/composables/useAuth.ts
+++ frontend/app/composables/useAuth.ts
@@ -18,6 +18,57 @@ export function useAuth() {
     }
   }
 
+  async function register(
+    email: string,
+    password: string,
+    firstname: string,
+    lastname: string,
+    pseudo: string,
+    userType: string,
+    ridingStartYear: string,
+    image?: string
+  ) {
+    await $fetch(`${apiBase}users/account`, {
+      method: 'POST',
+      body: {
+        email,
+        password,
+        firstname,
+        lastname,
+        pseudo,
+        userType,
+        ridingStartYear,
+        image
+      }
+    })
+    await login(email, password)
+  }
+
+  async function updateProfile(formData: FormData) {
+    // Extraire les données du FormData
+    const updateData: any = {}
+
+    // Ajouter uniquement les champs présents dans le FormData
+    if (formData.has('firstname'))
+      updateData.firstname = formData.get('firstname')
+    if (formData.has('lastname')) updateData.lastname = formData.get('lastname')
+    if (formData.has('pseudo')) updateData.pseudo = formData.get('pseudo')
+    if (formData.has('experience'))
+      updateData.userType = formData.get('experience')
+    if (formData.has('ridingStartYear'))
+      updateData.ridingStartYear = Number(formData.get('ridingStartYear'))
+    if (formData.has('email')) updateData.email = formData.get('email')
+    if (formData.has('image')) updateData.image = formData.get('image')
+
+    await $fetch(`${apiBase}users/account`, {
+      method: 'PUT',
+      credentials: 'include',
+      body: updateData
+    })
+
+    await fetchUser()
+  }
+
   async function login(email: string, password: string) {
     await $fetch(`${apiBase}auth`, {
       method: 'POST',
@@ -38,6 +89,8 @@ export function useAuth() {
   return {
     user: readonly(user),
     isAuthenticated,
+    register,
+    updateProfile,
     fetchUser,
     login,
     logout
